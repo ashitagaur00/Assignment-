5619b23dd75ffe3f36617590c47dada9f3a11c7a
Ashita Gaur <microsmall87@gmail.com>
2019-05-03 19:36:49 +0530

    Additonal files  removed

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { VgAPI } from 'videogular2/core';
+
 
 @Component({
   selector: 'app-dashboard',
